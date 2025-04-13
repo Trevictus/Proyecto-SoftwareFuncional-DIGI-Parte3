abd14b1e322d1933e0da55547520523bef182b58
VíctorG.T. <145797827+Trevictus@users.noreply.github.com>
2025-04-13 13:46:40 +0200

    Update Main.kt

--- src/Main.kt
+++ src/Main.kt
@@ -1,17 +1,18 @@
 import Connection.MySQL
 import Menu.Menu
 
+/**
+ * Main entry point for the application.
+ * Establishes a connection to the database, initializes the menu, and handles user interactions.
+ */
 fun main() {
+    val objeto = MySQL() // Create an instance of the MySQL connection handler.
 
-    val objeto = MySQL()
+    objeto.connect() // Connect to the MySQL database.
 
-    objeto.connect()
+    val menu = Menu(objeto) // Create an instance of the Menu class with the MySQL object.
 
-    val menu = Menu(objeto)
+    menu.elegirOpcion() // Display the menu and allow the user to choose an option.
 
-    menu.elegirOpcion()
-
-    objeto.disconnect()
-
-
+    objeto.disconnect() // Disconnect from the MySQL database.
 }
